b9dbcf20737f3b0c7c7d3e77cd32c5bddc825567
JianFGO <JianLin.L.He@student.shu.ac.uk>
2023-11-10 10:58:26 +0000

    xbf

--- Js/main.js
+++ Js/main.js
@@ -2,4 +2,6 @@
 document.getElementById('myHeading').innerHTML = "Jian lin";
 document.querySelector('nav ul li').setAttribute('class', 'currentPage');
 
+document.querySelector('nav ul li a').setAttribute('href', 
+'http://www.google.co.uk');
 })();
