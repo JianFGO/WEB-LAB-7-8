7345e4914f76f18a4eb4d2570454e3769c14f7b1
JianFGO <JianLin.L.He@student.shu.ac.uk>
2023-11-17 09:39:22 +0000

    seggdf

--- Js/main.js
+++ Js/main.js
@@ -5,3 +5,6 @@ document.querySelector('nav ul li').setAttribute('class', 'currentPage');
 document.querySelector('nav ul li a').setAttribute('href', 
 'http://www.google.co.uk');
 })();
+document.querySelector(".red").addEventListener('click', function(ev){
+ document.querySelector('body').setAttribute('class', "redBack");
+ })
