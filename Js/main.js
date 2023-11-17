8d346a3adbaadb91077caebe1cd7dddd1bccf94c
JianFGO <JianLin.L.He@student.shu.ac.uk>
2023-11-17 10:42:34 +0000

    dfs

--- Js/main.js
+++ Js/main.js
@@ -11,3 +11,17 @@ document.querySelector(".red").addEventListener('click', function(ev){
  document.querySelector('body').setAttribute('class', "redBack");
  })
 
+let imageAr = [
+    'images/view1.jpg', 
+    'images/view2.jpg', 
+    'images/view3.jpg',
+    'images/view4.jpg', 
+    'images/view5.jpg', 
+    'images/view6.jpg'];
+
+    let imageCount = 0;
+    setInterval(function() {
+        .getElementById("myImages")
+        .setAttribute("src", imageAr[imageCount]);
+        imageCount++;
+    }, 1000);
