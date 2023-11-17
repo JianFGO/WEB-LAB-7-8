832c92979819c754cc74376e89b54292b1bee9ef
JianFGO <JianLin.L.He@student.shu.ac.uk>
2023-11-17 10:29:33 +0000

    dsf

--- Js/main.js
+++ Js/main.js
@@ -6,10 +6,6 @@ document
 .querySelector('nav ul li a')
 .setAttribute('href', 'http://www.google.co.uk');
 })();
-document.getElementById('myTestEvent').onclick = myFunction;
-function myFunction(){
-console.info('hi');
-}
 
 document.querySelector(".red").addEventListener('click', function(){
  document.querySelector('body').setAttribute('class', "redBack");
