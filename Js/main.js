1c25515c7634a1e39b392f0e3d5a06beb931b16a
JianFGO <JianLin.L.He@student.shu.ac.uk>
2023-11-17 09:50:40 +0000

    jul

--- Js/main.js
+++ Js/main.js
@@ -5,6 +5,10 @@ document.querySelector('nav ul li').setAttribute('class', 'currentPage');
 document.querySelector('nav ul li a').setAttribute('href', 
 'http://www.google.co.uk');
 })();
+document.getElementById('myTestEvent').onclick = myFunction;
+function myFunction(){
+console.info('hi');
+}
 document.querySelector(".red").addEventListener('click', function(ev){
  document.querySelector('body').setAttribute('class', "redBack");
  })
